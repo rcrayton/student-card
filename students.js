94e93a2bb4a695b7817bd42023af829bdbe063e3
Rikki Crayton <rikkicrayton@TL-SG105PE.localdomain>
2022-04-12 01:52:58 -0400

    Added student objects

---
+++ students.js
@@ -0,0 +1,52 @@
+let students = [
+    {   
+        name: 'Lucas',
+        city: 'Garfield',
+        state: 'NJ',
+        hobbies: 'Playing Smash bros, going to the gym',
+        interests: 'Statistics and Basketball'
+    },
+    {   
+        name: 'Lucas',
+        city: 'Garfield',
+        state: 'NJ',
+        hobbies: 'Playing Smash bros, going to the gym',
+        interests: 'Statistics and Basketball'
+    },
+    {   
+        name: 'Lucas',
+        city: 'Garfield',
+        state: 'NJ',
+        hobbies: 'Playing Smash bros, going to the gym',
+        interests: 'Statistics and Basketball'
+    },
+    {   
+        name: 'Lucas',
+        city: 'Garfield',
+        state: 'NJ',
+        hobbies: 'Playing Smash bros, going to the gym',
+        interests: 'Statistics and Basketball'
+    },
+    {   
+        name: 'Lucas',
+        city: 'Garfield',
+        state: 'NJ',
+        hobbies: 'Playing Smash bros, going to the gym',
+        interests: 'Statistics and Basketball'
+    },
+    {   
+        name: 'Lucas',
+        city: 'Garfield',
+        state: 'NJ',
+        hobbies: 'Playing Smash bros, going to the gym',
+        interests: 'Statistics and Basketball'
+    },
+    {   
+        name: 'Lucas',
+        city: 'Garfield',
+        state: 'NJ',
+        hobbies: 'Playing Smash bros, going to the gym',
+        interests: 'Statistics and Basketball'
+    },
+]
+        
